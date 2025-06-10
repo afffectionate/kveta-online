1fb44d93166a862a47caa46b691f976b42d1c088
afffectionate <187199437+afffectionate@users.noreply.github.com>
2025-06-10 21:50:55 +0200

    add word cloud, edit graphics

--- server.py
+++ server.py
@@ -1,27 +1,45 @@
 from sanic import Sanic
 from sanic import response
-import regex
-
-FOLDER = "zvířátka"
-CONTEXT_SIZE = 50
+import spacy
+from collections import Counter
 
 app = Sanic("MyHelloWorldApp")
 app.static('/static', './static')
 
-def tokenize_text(text):
-  tokenized_text = regex.split("\\W+", text)
-  return tokenized_text
+nlp = spacy.load("en_core_web_sm")
+
+def preprocess_text(text):
+    doc = nlp(text)
+    return doc
+
+def tokenize_text(doc):
+    tokenized_text = [token.text for token in doc if not token.is_space and not token.is_punct]
+    return tokenized_text
 
 def get_types(tokens):
-   types = list(set(tokens))
-   return types
+    types = list(set(tokens))
+    return types
 
-def get_word_lengths(my_list):
-    lengths = []
-    for item in my_list:
-       lengths.append(len(item))
+def get_word_lengths(list_of_tokens):
+    lengths = [len(token) for token in list_of_tokens]
     return lengths
 
+def get_sentence_lengths(doc):
+    list_of_tokenized_sentences = [tokenize_text(sentence) for sentence in doc.sents]
+    sentence_lengths = [len(item) for item in list_of_tokenized_sentences]
+    return sentence_lengths
+
+def get_lemmas(doc):
+    lemmas = [token.lemma_ for token in doc if not token.is_space and not token.is_punct and not token.is_stop]
+    return lemmas
+
+def get_word_frequencies(lemmas):
+    return Counter(lemmas).most_common(50)
+
+def get_hapaxy(tokens):
+    freq = Counter(tokens)
+    return [word for word, count in freq.items() if count == 1]
+    
 @app.route("/", methods=["GET"])
 async def display_index_page(request):
     return await response.file("templates/index.html")
@@ -30,12 +48,19 @@ async def display_index_page(request):
 async def calculate_shit(request): 
     file = request.files.get("file")
     file_content = file.body.decode('utf-8')
-    tokens = tokenize_text(file_content)
+
+    doc = preprocess_text(file_content)
+    tokens = tokenize_text(doc)
     types = get_types(tokens)
+
     result_dict = {
         "tokens" : len(tokens),
         "types" : len(types),
         "ttr" : len(tokens)/len(types),
+        "wordLengths" : get_word_lengths(tokens),
+        "sentenceLengths" : get_sentence_lengths(doc),
+        "hapaxLegomena" : get_hapaxy(tokens),
+        "wordCloudData" : get_word_frequencies(get_lemmas(doc))
     }
     return response.json(result_dict)
 
